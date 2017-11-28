1af58c91c696d26cb410abd5263e7f9c2f4a38ba
Sergey Vasilenko <svasilenko@mirantis.com>
2017-11-29 00:18:34 +0300

    rename variables for match to 'generic' format

--- root/bin/test_case__2.sh
+++ root/bin/test_case__2.sh
@@ -5,21 +5,30 @@ if [ -n "${DEBUG}" ] ; then
     set -v
 fi
 
-if [ -z "${test_parameters__port}" ] ; then
+if [ -z "${test_parameters__server_port}" ] ; then
     PORT=""
 else
-    PORT=":${test_parameters__port}"
+    PORT=":${test_parameters__server_port}"
 fi
 
-if [ -z "${test_parameters__proto}" ] ; then
+if [ -z "${test_parameters__server_proto}" ] ; then
     PROTO="http"
 else
-    PROTO="${test_parameters__proto}"
+    PROTO="${test_parameters__server_proto}"
 fi
 
+if [ "${test_parameters__document_path}" == "/" ] || [ -z "${test_parameters__document_path}" ] ; then
+    URI="/" ;
+else
+    URI="${test_parameters__document_path}" ;
+fi
 
-ab -c ${test_parameters__concurrency} \
-   -n ${test_parameters__requests} \
-   ${PROTO}://${test_parameters__host}${PORT}/ > ${TEST_CASE_RESULT} ; rc=$?
+ab -c ${test_parameters__concurrency_level} \
+   -n ${test_parameters__requests_number} \
+   ${PROTO}://${test_parameters__server_hostname}${PORT}${URI} \
+   > ${TEST_CASE_RESULT} ; rc=$?
 
 exit $rc
+
+test_parameters__document_path="/"
+
